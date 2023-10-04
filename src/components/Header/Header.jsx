669d307ef6e8b44f185243642292cedcc195cea7
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2023-10-03 18:44:42 -0700

    Navbar created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 // Import the 'logo' image and styles
 import './Header.scss'
+import downloadIcon from '../../assests/icons/download.svg';
 
 // Import necessary React components and hooks
 import { useState } from 'react';
@@ -20,31 +21,29 @@ const Header = ({ isLoggedIn, setIsLoggedIn }) => {
 
     return (
         <header className="header">
-            <div className="header-container">
-                <div className="header__wrapper">
-                    <Link to='/' className="header__logo">Ahmad Akhtar</Link>
-                    <div className={menuOpen ? 'header__menu header__menu--close' : "header__menu"} onClick={() => {
-                        setMenuOpen(!menuOpen)
-                    }}></div>
-                </div>
-                <nav className={menuOpen ? 'navbar navbar--close' : "navbar"}>
-
-                    <ul className='navbar__list'>
-
-                        <li className="navbar__item">
-                            <Link to='/' onClick={menuClose} className="navbar__link">./About</Link>
-                        </li>
-                        <li className="navbar__item">
-                            <Link to='/tasks' onClick={menuClose} className="navbar__link">./Project</Link>
-                        </li>
-                        <li className="navbar__item">
-                            <Link to='login' onClick={menuClose} className="navbar__link">Resume</Link>
-                        </li>
-
-                    </ul>
-
-                </nav>
+            <div className="header__wrapper">
+                <Link to='/' className="header__logo">Ahmad Akhtar</Link>
+                <div className={menuOpen ? 'header__menu header__menu--close' : "header__menu"} onClick={() => {
+                    setMenuOpen(!menuOpen)
+                }}></div>
             </div>
+            <nav className={menuOpen ? 'navbar navbar--close' : "navbar"}>
+
+                <ul className='navbar__list'>
+
+                    <li className="navbar__item">
+                        <Link to='/' onClick={menuClose} className="navbar__link">./About</Link>
+                    </li>
+                    <li className="navbar__item">
+                        <Link to='/' onClick={menuClose} className="navbar__link">./Project</Link>
+                    </li>
+                    <li className="navbar__item">
+                        <button onClick={menuClose} className="navbar__link navbar__link--download"><img src={downloadIcon} alt="" />Resume</button>
+                    </li>
+
+                </ul>
+
+            </nav>
         </header>
     )
 }
