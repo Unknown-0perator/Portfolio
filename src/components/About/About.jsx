b358342092152a268993d39b231c5ae57a163781
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2023-10-05 20:26:17 -0700

    skills animation created

---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+import './About.scss';
+
+const About = () => {
+    return (
+        <section className="about">
+            <h2 className="about__heading">About</h2>
+            <p className="about__content">Hello, I’m Ahmad Akhtar, recent Computer Science graduate with a strong passion for web development, as evidenced by my successful projects and coursework. I am proactive in seeking out new challenges and taking the initiative to lead. Alongside my technical skills, I prioritize effective communication and enjoy fostering connections, showcasing my commitment to collaborative and productive environments.</p>
+        </section>
+    )
+}
+
+export default About;
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,13 +1,18 @@
 import './LandingPage.scss'
 import Header from '../../components/Header/Header';
 import Banner from '../../components/Banner/Banner';
+import Divider from '../../components/Divider/Divider';
+import About from '../../components/About/About';
+import Skill from '../../components/Skill/Skill';
 
 const LandingPage = () => {
     return (
         <>
             <Header />
             <Banner />
-            <div className="divider"></div>
+            <Divider />
+            <About />
+            <Skill />
         </>
     )
 }
